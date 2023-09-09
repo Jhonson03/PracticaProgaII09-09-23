6b697f5e62f7b0aef38c749e9b064d97f572a152
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-09-09 14:10:57 -0600

    ..

---
+++ PracticaS6/Program.cs
@@ -0,0 +1,21 @@
+﻿// Practica
+
+String[] Frutas = { "Melon", "Fresa", "Manzana", "Guayaba", "Uva" };
+
+var Orden = from x in Frutas select x;
+
+Console.WriteLine(string.Join(" ", Orden));
+
+/**************************************************************/
+
+IEnumerable<string> OrdenAscendente = from x in Frutas orderby x select x;
+
+Console.WriteLine($"Ascendente: {string.Join(" ", OrdenAscendente)}");
+
+IEnumerable<string> OrdenDescendente = from x in Frutas orderby x descending select x;
+
+Console.WriteLine($"Descendente: {string.Join(" ", OrdenDescendente)}");
+
+IEnumerable<string> Finalice = from x in Frutas where x.EndsWith("na") select x;
+Console.WriteLine($"Finalice: {string.Join(" ", Finalice)}");
+
